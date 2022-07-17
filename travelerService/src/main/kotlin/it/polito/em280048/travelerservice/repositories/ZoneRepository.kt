71dfba6953ffe3d624e309cce81dbc530372c2f7
aile96 <missud2@gmail.com>
2022-07-17 18:05:45 +0200

    Final Commit

--- travelerService/src/main/kotlin/it/polito/em280048/travelerservice/repositories/ZoneRepository.kt
+++ travelerService/src/main/kotlin/it/polito/em280048/travelerservice/repositories/ZoneRepository.kt
@@ -2,7 +2,6 @@ package it.polito.em280048.travelerservice.repositories
 
 import it.polito.em280048.travelerservice.entities.Zone
 import org.springframework.data.repository.CrudRepository
-import java.util.*
 
 interface ZoneRepository : CrudRepository<Zone, Long> {
     fun findByZoneName(name: String): Zone?
