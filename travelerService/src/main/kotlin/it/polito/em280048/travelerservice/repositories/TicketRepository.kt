debefb250ac40b002d6516a0adb22dd4d90e5920
aile96 <missud2@gmail.com>
2022-07-17 18:07:22 +0200

    Final Commit

--- travelerService/src/main/kotlin/it/polito/em280048/travelerservice/repositories/TicketRepository.kt
+++ travelerService/src/main/kotlin/it/polito/em280048/travelerservice/repositories/TicketRepository.kt
@@ -5,6 +5,6 @@ import org.springframework.data.repository.CrudRepository
 
 interface TicketRepository : CrudRepository<Ticket, Long> {
     fun findAllByUserDetailsId(userDetails_id: Long): MutableList<Ticket>
-    fun findAllByTicketsZoneHash(ticketsZone_hash: String): MutableList<Ticket>
-    fun findTop1ByOrderByTicketsZoneIdentifierDesc(): Ticket?
+    //fun findAllByTicketsZoneHash(ticketsZone_hash: String): MutableList<Ticket>
+    //fun findTop1ByOrderByTicketsZoneIdentifierDesc(): Ticket?
 }
